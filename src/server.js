e7efb4c22483ff9b7af9dcabf6553ef163a83816
Seokgyu Lee <dltjrrb0729@naver.com>
2020-02-19 23:53:31 +0900

    before prisma

--- src/server.js
+++ src/server.js
@@ -1,20 +1,13 @@
 console.log("hello");
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-    type Query{
-        hello:String!
-    }
-`;
-const resolvers = {
-  Query: {
-    hello: () => "Hi"
-  }
-};
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({ schema });
+server.express.use(logger("dev"));
 
 console.log("check");
 server.start({ port: PORT }, () =>
