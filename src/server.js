34ba73f0f5d66a89e08067ae9e954a29783f940a
Seokgyu Lee <dltjrrb0729@naver.com>
2020-02-19 22:13:44 +0900

    developed simple graphql server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,22 @@
 console.log("hello");
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello:String!
+    }
+`;
+const resolvers = {
+  Query: {
+    hello: () => "Hi"
+  }
+};
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+console.log("check");
+server.start({ port: PORT }, () =>
+  console.log(`The Server is running on http://localhost:${PORT}`)
+);
