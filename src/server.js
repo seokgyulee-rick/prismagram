a3721559e780a7814b5bca67d8db7a2389beae72
seokgyulee <dltjrrb0729@naver.com>
2020-03-06 21:04:08 +0900

    3.3 middle

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-console.log("hello");
-require("dotenv").config();
+import dotevnv from "dotenv";
+import path from "path";
+dotevnv.config({ path: path.resolve(__dirname, ".env") });
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 
+console.log(process.env.PORT);
 const PORT = process.env.PORT || 4000;
 
 const server = new GraphQLServer({ schema });
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,24 @@
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  auth: {
+    api_key: 'key-1234123412341234',
+    domain: 'one of your domain names listed at your https://mailgun.com/app/domains'
+  }
+}
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "nico@prismagram.com",
+    to: adress,
+    subject: "Login Secret for Prismagram",
+    html: `Hello! Your login secret is ${secret}.<br/> Copy Paste on the app/web`
+  };
 };
